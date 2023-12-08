bdada482afb5a919b747a3400b091492f7b349be
wilian santos <14809032647@estudante.ufjf.br>
2023-12-07 21:55:10 -0300

    altera funcoes de jogar o dado para os jogadores

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 // main.js
 import { gerarNumero, gerarNumeroPc } from './jogo.js';
 
-document.addEventListener('DOMContentLoaded', main);
 
-function main() {
     const gerarNumeroBtn = document.getElementById('gerarNumeroBtn');
     gerarNumeroBtn.addEventListener('click', gerarNumero);
 
     const gerarNumeroBtnPc = document.getElementById('gerarNumeroBtnPc');
     gerarNumeroBtnPc.addEventListener('click', gerarNumeroPc);
-}
+
