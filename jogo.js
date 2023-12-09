f772dbf823871e72a8cefac0f1efb6fe0555d767
wilian santos <14809032647@estudante.ufjf.br>
2023-12-09 14:48:28 -0300

    cria condição para a armazenar o numero na celula vazia

--- jogo.js
+++ jogo.js
@@ -6,15 +6,44 @@ function gerarNumeroAleatorio() {
 export function gerarNumero() {
     const numeroAleatorio = gerarNumeroAleatorio();
     const celulas = document.querySelectorAll('.area-jogador1 table td');
-    const celulaAleatoria = celulas[Math.floor(Math.random() * celulas.length)];
-    celulaAleatoria.textContent = numeroAleatorio;
-    document.getElementById('resultado').textContent = 'Resultado: ' + numeroAleatorio;
+
+
+    
+    const celulasVazias = Array.from(celulas).filter(celula => !celula.textContent.trim());
+
+
+    if (celulasVazias.length > 0) {
+        // Escolhe aleatoriamente uma célula vazia 
+        const celulaAleatoria = celulasVazias[Math.floor(Math.random() * celulasVazias.length)];
+
+        // conteúdo da célula escolhida recebe o numero aleatorio 
+        celulaAleatoria.textContent = numeroAleatorio;
+
+        // Atualiza o conteúdo do elemento com o ID 'resultado'
+        document.getElementById('resultado').textContent = 'Resultado: ' + numeroAleatorio;
+    } else {
+        console.log('Todas as células estão ocupadas.'); 
+    }
+
 }
 
 export function gerarNumeroPc() {
     const numeroAleatorioPc = gerarNumeroAleatorio();
     const celulasPc = document.querySelectorAll('.area-jogador  table td');
-    const celulaAleatoriaPc = celulasPc[Math.floor(Math.random() * celulasPc.length)];
-    celulaAleatoriaPc.textContent = numeroAleatorioPc;
-    document.getElementById('resultadoPc').textContent = 'Resultado: ' + numeroAleatorioPc;
+    const celulasVazias = Array.from(celulasPc).filter(celula => !celula.textContent.trim());
+
+
+    if (celulasVazias.length > 0) {
+        // Escolhe aleatoriamente uma célula vazia 
+        const celulaAleatoria = celulasVazias[Math.floor(Math.random() * celulasVazias.length)];
+
+        // conteúdo da célula escolhida recebe o numero aleatorio 
+        celulaAleatoria.textContent = numeroAleatorioPc;
+
+        // Atualiza o conteúdo do elemento com o ID 'resultado'
+        document.getElementById('resultadoPc').textContent = 'Resultado: ' + numeroAleatorioPc;
+    } else {
+        console.log('Todas as células estão ocupadas.'); 
+    }
+
 }
