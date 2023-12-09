9d4649ebbd7d04722093625953ce8364a2c86674
wilian santos <14809032647@estudante.ufjf.br>
2023-12-08 21:50:00 -0300

    seleciona celula aleatoria

--- jogo.js
+++ jogo.js
@@ -5,10 +5,16 @@ function gerarNumeroAleatorio() {
 
 export function gerarNumero() {
     const numeroAleatorio = gerarNumeroAleatorio();
+    const celulas = document.querySelectorAll('.area-jogador1 table td');
+    const celulaAleatoria = celulas[Math.floor(Math.random() * celulas.length)];
+    celulaAleatoria.textContent = numeroAleatorio;
     document.getElementById('resultado').textContent = 'Resultado: ' + numeroAleatorio;
 }
 
 export function gerarNumeroPc() {
     const numeroAleatorioPc = gerarNumeroAleatorio();
+    const celulasPc = document.querySelectorAll('.area-jogador  table td');
+    const celulaAleatoriaPc = celulasPc[Math.floor(Math.random() * celulasPc.length)];
+    celulaAleatoriaPc.textContent = numeroAleatorioPc;
     document.getElementById('resultadoPc').textContent = 'Resultado: ' + numeroAleatorioPc;
 }
